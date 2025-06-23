7baec59eb2af683ac173f61fdb115d80b753c009
Loc Ngo-Huynh-Tan <locngohuynhtan@gmail.com>
2025-06-23 09:01:51 +0700

    feat: delete

--- api/Controllers/StockController.cs
+++ api/Controllers/StockController.cs
@@ -63,7 +63,21 @@ namespace api.Controllers
             stockModel.MarketCap = updateDto.MarketCap;
 
             _context.SaveChanges();
-            return Ok(stockModel.ToStockDto()); 
+            return Ok(stockModel.ToStockDto());
+        }
+
+        [HttpDelete]
+        [Route("{id}")]
+        public IActionResult Delete([FromRoute] int id)
+        {
+            var stockModel = _context.Stocks.FirstOrDefault(s => s.Id == id);
+            if (stockModel == null)
+            {
+                return NotFound();
+            }
+            _context.Stocks.Remove(stockModel);
+            _context.SaveChanges();
+            return NoContent();
         }
     }
 }
